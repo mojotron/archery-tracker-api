8f97ecdd3a80a8e6ac8a2535bb8a6e91875d67da
mojo <igormalj@gmail.com>
2025-05-12 20:03:18 +0200

    add contatnts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
+import "dotenv/config";
 import express from "express";
+import { PORT, NODE_ENV } from "./constants/env.js";
+
+const app = express();
+
 const startServer = async () => {
   try {
-  } catch (error) {}
+    app.listen(PORT, () =>
+      console.log(`server running on port ${PORT} in ${NODE_ENV} environment`)
+    );
+  } catch (error) {
+    process.exit(1);
+  }
 };
 
 startServer();
