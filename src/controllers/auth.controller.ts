7b42acf4b7ec5a7a82e80b0dedbc23e756a56067
mojo <igormalj@gmail.com>
2025-05-18 20:07:18 +0200

    add some boilerplate

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -7,3 +7,31 @@ export const registerHandler = catchErrors(async (req, res) => {
     userAgent: req.headers["user-agent"],
   });
 });
+
+export const loginHandler = catchErrors(async (req, res) => {
+  const request = registerSchema.parse({
+    ...req.body,
+    userAgent: req.headers["user-agent"],
+  });
+});
+
+export const logoutHandler = catchErrors(async (req, res) => {
+  const request = registerSchema.parse({
+    ...req.body,
+    userAgent: req.headers["user-agent"],
+  });
+});
+
+export const resetPasswordHandler = catchErrors(async (req, res) => {
+  const request = registerSchema.parse({
+    ...req.body,
+    userAgent: req.headers["user-agent"],
+  });
+});
+
+export const passwordHandler = catchErrors(async (req, res) => {
+  const request = registerSchema.parse({
+    ...req.body,
+    userAgent: req.headers["user-agent"],
+  });
+});
