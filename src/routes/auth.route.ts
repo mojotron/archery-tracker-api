67ecd0f9dc3ef231017eec4e3e09198f01fc95b1
mojo <igormalj@gmail.com>
2025-05-19 15:11:44 +0200

    add

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { registerHandler } from "../controllers/auth.controller.js";
+import {
+  registerHandler,
+  loginHandler,
+  logoutHandler,
+} from "../controllers/auth.controller.js";
 
 const authRoutes = Router();
 
 authRoutes.post("/register", registerHandler);
+authRoutes.post("/login", registerHandler);
+authRoutes.post("/logout", registerHandler);
+authRoutes.post("/password/reset", registerHandler);
 
 export default authRoutes;
